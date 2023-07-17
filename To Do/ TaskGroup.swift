416fbf6985d03466e4bc492f52e9d7fab3daf111
Matthew Lundeen <decentishdev@gmail.com>
2023-07-17 15:33:41 -0500

    taskgroups progress

--- To Do/ TaskGroup.swift
+++ To Do/ TaskGroup.swift
@@ -10,12 +10,14 @@ import Foundation
 class TaskGroup {
     let defaults = UserDefaults.standard
     let ID: Int
+    let title: String
     let senderTasks: [Int]
     let nextTasks: [Int]
     let tasks: [String]
     
-    init(ID: Int, senderTasks: [Int], nextTasks: [Int], tasks: [String]) {
+    init(ID: Int, title: String, senderTasks: [Int], nextTasks: [Int], tasks: [String]) {
         self.ID = ID
+        self.title = title
         self.senderTasks = senderTasks
         self.nextTasks = nextTasks
         self.tasks = tasks
--- To Do/FlowchartVC.swift
+++ To Do/FlowchartVC.swift
@@ -28,169 +28,164 @@ class FlowchartVC: UIViewController {
             defaults.set(FlowchartVC.taskGroups, forKey: "taskGroups")
         }
         
-        let group1 = TaskGroup(ID: 0, senderTasks: [], nextTasks: [1], tasks: ["Group 1 task 1", "Group 1 task 2"])
-        let group2 = TaskGroup(ID: 1, senderTasks: [0], nextTasks: [2], tasks: ["Group 2 task 1", "Group 2 task 2"])
-        let group3 = TaskGroup(ID: 2, senderTasks: [1], nextTasks: [3], tasks: ["Group 3 task 1", "Group 3 task 2"])
-        let group4 = TaskGroup(ID: 3, senderTasks: [2], nextTasks: [], tasks: ["Group 4 task 1", "Group 4 task 2"])
+        let group1 = TaskGroup(ID: 0, title: "Group 1", senderTasks: [], nextTasks: [2], tasks: ["Group 1 task 1", "Group 1 task 2", "Group 1 task 3"])
+        let group2 = TaskGroup(ID: 1, title: "Group 2", senderTasks: [], nextTasks: [3], tasks: ["Group 2 task 1", "Group 2 task 2"])
+        let group3 = TaskGroup(ID: 2, title: "Group 3", senderTasks: [1], nextTasks: [], tasks: ["Group 3 task 1", "Group 3 task 2"])
+        let group4 = TaskGroup(ID: 3, title: "Group 4", senderTasks: [2], nextTasks: [], tasks: ["Group 4 task 1", "Group 4 task 2"])
         FlowchartVC.taskGroups = [group1, group2, group3, group4]
-        generateGroups()
-
+        
+        var c: [Int] = []
+        for (i, group) in FlowchartVC.taskGroups.enumerated() {
+            if(group.senderTasks == []){
+                c.append(i)
+            }
+        }
+        for i in 0 ..< c.count {
+            let stackView = generateGroup(taskGroup: FlowchartVC.taskGroups[c[i]])
+            viewHolder.addSubview(stackView)
+            stackView.leadingAnchor.constraint(equalTo: viewHolder.leadingAnchor, constant: 100).isActive = true //+350
+            stackView.topAnchor.constraint(equalTo: viewHolder.topAnchor, constant: CGFloat(300 * (i + 1))).isActive = true
+            generateNextGroup(lastX: 100, lastY: 300 * (i + 1), lastGroup: FlowchartVC.taskGroups[c[i]])
+        }
+        
+        
+        view.addSubview(viewHolder)
+        
+        
         // Enable user interaction and add a gesture recognizer to the view
         view.isUserInteractionEnabled = true
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
         view.addGestureRecognizer(panGesture)
     }
-
-    // Method to add rectangles to the view
-//    func generateGroups() {
-//        // Create rectangles with different colors and sizes
-//        let center = view.center
-//
-//        for i in 0..<FlowchartVC.taskGroups.count {
-//            let rectangle = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//            rectangle.backgroundColor = UIColor.white
-//            rectangle.layer.cornerRadius = 10
-//            rectangle.center = CGPoint(x: center.x + CGFloat(i * 150), y: center.y)
-//            view.addSubview(rectangle)
-//            rectangles.append(rectangle)
-//        }
-//    }
-    func generateGroups() {
-        // Create a stack view
-            let stackView = UIStackView()
-            stackView.axis = .vertical
-            stackView.alignment = .fill
-            stackView.distribution = .fill
-            stackView.spacing = 10 // Adjust spacing as needed
-            
-            // Add a background view to the stack view
-            let backgroundView = UIView()
+    
+    func generateNextGroup(lastX: Int, lastY: Int, lastGroup: TaskGroup) {
+        var nextGroups: [TaskGroup] = []
+        for i in lastGroup.nextTasks {
+            for g in FlowchartVC.taskGroups {
+                if(i == g.ID){
+                    nextGroups.append(g)
+                }
+            }
+        }
+        for (i, group) in nextGroups.enumerated() {
+            let stackView = generateGroup(taskGroup: group)
+            viewHolder.addSubview(stackView)
+            stackView.leadingAnchor.constraint(equalTo: viewHolder.leadingAnchor, constant: CGFloat(lastX + 200)).isActive = true //+350
+            let newYPos = lastY + 300 * ((Double(i) / Double(nextGroups.count - 1)) - 0.5))
+            stackView.topAnchor.constraint(equalTo: viewHolder.topAnchor, constant: CGFloat(newYPos).isActive = true
+            generateNextGroup(lastX: 100, lastY: newYPos, lastGroup: group)
+        }
+    }
+    
+    func generateGroup(taskGroup: TaskGroup) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        let backgroundView = UIView()
         backgroundView.backgroundColor = UIColor.secondarySystemBackground
         backgroundView.layer.cornerRadius = 16
-            stackView.addSubview(backgroundView)
-        
+        stackView.addSubview(backgroundView)
+    
         let groupTitleLabel = UILabel()
-            groupTitleLabel.text = "Group Title"
-            groupTitleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold) // Adjust the font size and weight as desired
+        groupTitleLabel.text = taskGroup.title
+        groupTitleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         groupTitleLabel.textColor = UIColor.secondaryLabel
-            groupTitleLabel.textAlignment = .left
-            groupTitleLabel.numberOfLines = 0 // Allow multiple lines if needed
-            groupTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-            
-            // Add the settings image view
-            let settingsImageView = UIImageView(image: UIImage(systemName: "gearshape.fill")) // Replace "settings" with the actual name of your image
-            settingsImageView.contentMode = .scaleAspectFit
-            settingsImageView.translatesAutoresizingMaskIntoConstraints = false
-            
-            // Create a container view for the group title and settings image
-            let groupTitleContainerView = UIView()
-            groupTitleContainerView.translatesAutoresizingMaskIntoConstraints = false
-            
-            // Add the group title label and settings image view to the container view
-            groupTitleContainerView.addSubview(groupTitleLabel)
-            groupTitleContainerView.addSubview(settingsImageView)
+        groupTitleLabel.textAlignment = .left
+        groupTitleLabel.numberOfLines = 0
+        groupTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        // Configure auto layout constraints for the group title container view
+        let settingsImageView = UIImageView(image: UIImage(systemName: "gearshape.fill"))
+        settingsImageView.contentMode = .scaleAspectFit
+        settingsImageView.translatesAutoresizingMaskIntoConstraints = false
+        settingsImageView.tintColor = UIColor.secondaryLabel
+        
+        let groupTitleContainerView = UIView()
+        groupTitleContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        groupTitleContainerView.addSubview(groupTitleLabel)
+        groupTitleContainerView.addSubview(settingsImageView)
+        
+        
+        stackView.addSubview(groupTitleContainerView)
+        let stackViewWidth: CGFloat = 280
+        
+        for labelText in taskGroup.tasks {
+            // Create a container view for each label
+            let containerView = UIView()
+            containerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // Create a small circle view
+            let circleView = UIView()
+            circleView.backgroundColor = .clear
+            circleView.layer.borderColor = UIColor.label.cgColor
+            circleView.layer.borderWidth = 2
+            circleView.translatesAutoresizingMaskIntoConstraints = false
+            circleView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+            circleView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+            circleView.layer.cornerRadius = 10 // Adjust the radius to control the circle size
+            
+            containerView.addSubview(circleView)
+            
+            // Create the label
+            let label = UILabel()
+            label.text = labelText
+            label.textColor = UIColor.label
+            label.numberOfLines = 0 // Allow multiple lines if needed
+            label.translatesAutoresizingMaskIntoConstraints = false
+            
+            containerView.addSubview(label)
+            
+            // Configure auto layout constraints for the container view
             NSLayoutConstraint.activate([
-                groupTitleLabel.leadingAnchor.constraint(equalTo: groupTitleContainerView.leadingAnchor),
-                groupTitleLabel.topAnchor.constraint(equalTo: groupTitleContainerView.topAnchor),
-                groupTitleLabel.bottomAnchor.constraint(equalTo: groupTitleContainerView.bottomAnchor),
+                circleView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+                circleView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
                 
-                settingsImageView.leadingAnchor.constraint(equalTo: groupTitleLabel.trailingAnchor, constant: 8),
-                settingsImageView.trailingAnchor.constraint(equalTo: groupTitleContainerView.trailingAnchor),
-                settingsImageView.centerYAnchor.constraint(equalTo: groupTitleLabel.centerYAnchor),
-                settingsImageView.widthAnchor.constraint(equalToConstant: 20), // Adjust the width as desired
-                settingsImageView.heightAnchor.constraint(equalToConstant: 20) // Adjust the height as desired
+                label.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 8),
+                label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+                label.topAnchor.constraint(equalTo: containerView.topAnchor),
+                label.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
             ])
             
-            // Add the group title container view above the stack view
-            stackView.addSubview(groupTitleContainerView)
-            // Create a constant width for the stack view
-            let stackViewWidth: CGFloat = 280 // Adjust the width as needed
+            stackView.addArrangedSubview(containerView)
+        }
+        
+        // Configure auto layout constraints for the stack view
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // Stack view constraints
+            stackView.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: stackViewWidth),
             
-            // Add labels to the stack view
-            let labels = [
-                "First label",
-                "Second label with a longer text that will span multiple lines. This is an example of a long text that will wrap to multiple lines when it exceeds the available width of the label.",
-                "Third label",
-                "Fourth label",
-                "Maybe like around two lines for label number five",
-                "six"
-            ]
+            // Background view constraints
+            backgroundView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
+            backgroundView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
+            backgroundView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -8),
+            backgroundView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 8),
             
-            for labelText in labels {
-                // Create a container view for each label
-                let containerView = UIView()
-                containerView.translatesAutoresizingMaskIntoConstraints = false
-                
-                // Create a small circle view
-                let circleView = UIView()
-                circleView.backgroundColor = .clear
-                circleView.layer.borderColor = UIColor.label.cgColor
-                circleView.layer.borderWidth = 2
-                circleView.translatesAutoresizingMaskIntoConstraints = false
-                circleView.widthAnchor.constraint(equalToConstant: 20).isActive = true
-                circleView.heightAnchor.constraint(equalToConstant: 20).isActive = true
-                circleView.layer.cornerRadius = 10 // Adjust the radius to control the circle size
-                
-                containerView.addSubview(circleView)
-                
-                // Create the label
-                let label = UILabel()
-                label.text = labelText
-                label.textColor = UIColor.label
-                label.numberOfLines = 0 // Allow multiple lines if needed
-                label.translatesAutoresizingMaskIntoConstraints = false
-                
-                containerView.addSubview(label)
-                
-                // Configure auto layout constraints for the container view
-                NSLayoutConstraint.activate([
-                    circleView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
-                    circleView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
-                    
-                    label.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 8),
-                    label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-                    label.topAnchor.constraint(equalTo: containerView.topAnchor),
-                    label.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
-                ])
-                
-                stackView.addArrangedSubview(containerView)
-            }
+            // Group title label constraints
+            groupTitleContainerView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            groupTitleContainerView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -40),
+            groupTitleContainerView.trailingAnchor.constraint(lessThanOrEqualTo: stackView.trailingAnchor),
             
-            // Configure auto layout constraints for the stack view
-            stackView.translatesAutoresizingMaskIntoConstraints = false
-            backgroundView.translatesAutoresizingMaskIntoConstraints = false
-            
-            NSLayoutConstraint.activate([
-                // Stack view constraints
-                stackView.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
-                stackView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
-                stackView.widthAnchor.constraint(equalToConstant: stackViewWidth),
-                
-                // Background view constraints
-                backgroundView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
-                backgroundView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
-                backgroundView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -8),
-                backgroundView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 8),
-                
-                // Group title label constraints
-                groupTitleContainerView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
-                        groupTitleContainerView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -40),
-                        groupTitleContainerView.trailingAnchor.constraint(lessThanOrEqualTo: stackView.trailingAnchor, constant: -16)
-            ])
-        viewHolder.addSubview(stackView)
-        view.addSubview(viewHolder)
-        viewHolder.layer.position = view.center
-        stackView.center = viewHolder.convert(viewHolder.center, from:viewHolder.superview)
+            groupTitleLabel.leadingAnchor.constraint(equalTo: groupTitleContainerView.leadingAnchor),
+            groupTitleLabel.topAnchor.constraint(equalTo: groupTitleContainerView.topAnchor),
+            groupTitleLabel.bottomAnchor.constraint(equalTo: groupTitleContainerView.bottomAnchor),
+           
+            settingsImageView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            settingsImageView.centerYAnchor.constraint(equalTo: groupTitleLabel.centerYAnchor),
+            settingsImageView.widthAnchor.constraint(equalToConstant: 20),
+            settingsImageView.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        return stackView
     }
 
-
-
-
-
-
-    // Method to handle the pan gesture
     @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
         let touchPoint = gestureRecognizer.location(in: self.view)
 
